6264d23fe1639f67163a17c77a60fbe56feeb956
Sadia Binta Kabir <sadiaa.kabir@gmail.com>
2023-03-29 14:17:06 -0400

    cart added

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Product.css'
 
 const Product = (props) => {
-    const {img,name,seller,ratings,price}=props.product;
+    //console.log(props)
+    const { img, name, seller, ratings, price } = props.product;
+    const handleAddToCart = props.handleAddToCart;
+
     return (
         <div className='product'>
             <img src={img} alt="" />
-            <div class='product-info'>
-            <h6 class='product-name'>{name}</h6>
-            <p>Price: ${price}</p>
-            <p>Manufactures: {seller}</p>
-            <p>Rating: {ratings} Stars</p>
+            <div className='product-info'>
+                <h6 className='product-name'>{name}</h6>
+                <p>Price: ${price}</p>
+                <p>Manufactures: {seller}</p>
+                <p>Rating: {ratings} Stars</p>
             </div>
-            <button className='btn-cart'>Add to cart</button>
+            <button onClick={() => handleAddToCart(props.product)} className='btn-cart'>
+                Add to cart <FontAwesomeIcon icon={faShoppingCart} />
+                </button>
         </div>
     );
 };
